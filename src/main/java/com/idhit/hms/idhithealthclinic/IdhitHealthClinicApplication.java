7f2f562942e2b8a841ddbf20380e2b23bf230e04
Dhruv Singh K <dhruvsinghk@Dhruvs-MacBook-Pro.local>
2022-06-21 22:05:05 +0530

    First commit of application creation - 21/06 10:05

---
+++ src/main/java/com/idhit/hms/idhithealthclinic/IdhitHealthClinicApplication.java
@@ -0,0 +1,13 @@
+package com.idhit.hms.idhithealthclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdhitHealthClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdhitHealthClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/idhit/hms/idhithealthclinic/IdhitHealthClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idhit.hms.idhithealthclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdhitHealthClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
